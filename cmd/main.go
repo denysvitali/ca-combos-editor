fb90ff0ebed03cb55ed8ff3de29f3d7e2d38150e
Denys Vitali <denys.vitali@nanos.ai>
2019-11-17 22:18:32 +0100

    README: Improve documentation

--- cmd/main.go
+++ cmd/main.go
@@ -22,9 +22,9 @@ type ParseCmd struct {
 }
 
 var args struct {
-	Create *CreateCmd `arg:"subcommand:create"`
-	Create2 *Create2Cmd `arg:"subcommand:create2"`
-	Parse *ParseCmd `arg:"subcommand:parse"`
+	Create     *CreateCmd  `arg:"subcommand:create"`
+	CreateDlUl *Create2Cmd `arg:"subcommand:create_dlul"`
+	Parse      *ParseCmd   `arg:"subcommand:parse"`
 }
 
 func main(){
@@ -35,15 +35,15 @@ func main(){
 	case args.Create != nil:
 		entries := pkg.ParseBandFile(args.Create.Input)
 		pkg.WriteComboFile(entries, args.Create.Output)
-	case args.Create2 != nil:
-		entries := pkg.ParseBandDLULFile(args.Create2.Downlink, args.Create2.Uplink)
+	case args.CreateDlUl != nil:
+		entries := pkg.ParseBandDLULFile(args.CreateDlUl.Downlink, args.CreateDlUl.Uplink)
 		for _, e := range entries {
 			if e.Name() == "DL" {
 				pkg.Log.Debugf("\n")
 			}
 			pkg.Log.Debugf("%s: %s\n", e.Name(), e)
 		}
-		pkg.WriteComboFile(entries, args.Create2.Output)
+		pkg.WriteComboFile(entries, args.CreateDlUl.Output)
 	case args.Parse != nil:
 		pkg.ReadComboFile(args.Parse.Input)
 	default:
